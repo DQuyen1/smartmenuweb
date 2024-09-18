eb53e7f88352a5936c5ad912d486185463d57439
DQuyen231 <98691014+DQuyen231@users.noreply.github.com>
2024-09-18 13:08:18 +0700

    update router handle netlify

--- src/utils/canvasFeatures.js
+++ src/utils/canvasFeatures.js
@@ -22,6 +22,41 @@ class canvasFeatures {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return `#${randomColor.padStart(6, '0')}`;
   }
+
+  getProductContentValue(boxItemType) {
+    let value = '';
+
+    switch (boxItemType) {
+      case 1:
+        value = 'Header';
+        break;
+
+      case 2:
+        value = 'N';
+        break;
+
+      case 3:
+        value = 'D';
+        break;
+
+      case 4:
+        value = 'P';
+        break;
+
+      case 5:
+        value = 'Img';
+        break;
+
+      case 6:
+        value = 'Icon';
+        break;
+
+      default:
+        break;
+    }
+
+    return value;
+  }
 }
 
 export default canvasFeatures;
